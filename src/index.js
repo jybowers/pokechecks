66cae4e0df27a0da2b7cb524253230af41d8b41c
Joshua Bowers <jybowers@protonmail.com>
2020-07-25 15:34:08 -0500

    Initial setup. Added outline for first draft of what the poke-card will look like.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import Pokemon from './Pokemon';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Pokemon />
   </React.StrictMode>,
   document.getElementById('root')
 );
